2c7ea991be2612ec4e993b44fb96c5a2307d594a
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-16 11:47:03 +0430

    init cnf

---
+++ src/main/java/af/asr/authadapter/AuthAdapterApplication.java
@@ -0,0 +1,13 @@
+package af.asr.authadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/asr/authadapter/AuthAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.authadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
